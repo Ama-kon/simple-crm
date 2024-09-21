1d5cb935d54ac50a1350a1487dc6b7c61e4ab057
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-21 21:39:01 +0200

    functions -to save user / -to clear form

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,5 +1,5 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, inject } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -8,6 +8,9 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -21,19 +24,34 @@ import { FormsModule } from '@angular/forms';
     MatDatepickerModule,
     MatButtonModule,
     FormsModule,
+    MatProgressBarModule,
+    CommonModule,
   ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
 })
 export class DialogAddUserComponent {
   user = new User();
   birthDate: Date;
+  loading = false;
+  private firestore: Firestore = inject(Firestore);
+  constructor(public dialog: MatDialogRef<DialogAddUserComponent>) {}
 
-  constructor() {}
-
-  saveUser() {
+  async saveUser() {
+    this.loading = true;
     this.user.birthDate = this.birthDate.getTime();
-    console.log('User:', this.user);
+    const userCollection = collection(this.firestore, 'users');
+    const userToJson = Object.assign({}, this.user);
+    await addDoc(userCollection, userToJson);
+    this.clearForm();
+    this.loading = false;
+    this.dialog.close();
+  }
+
+  clearForm() {
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach((input) => {
+      input.value = '';
+    });
   }
 }
