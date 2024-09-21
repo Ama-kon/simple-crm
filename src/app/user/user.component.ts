9c9f64f0e9f4280ef76c2a32bf7f4cea853a9419
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-21 21:39:52 +0200

    removed unneeded variable

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,16 @@
-import { Component, inject, model, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatIcon } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIcon, MatButtonModule, MatTooltipModule],
+  imports: [MatIcon, MatButtonModule, MatTooltipModule, MatCardModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
@@ -18,7 +19,7 @@ export class UserComponent {
   constructor(public dialog: MatDialog) {}
 
   openDialog(): void {
-    const dialogRef = this.dialog.open(DialogAddUserComponent, {
+    this.dialog.open(DialogAddUserComponent, {
       data: {},
     });
   }
