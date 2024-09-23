1872efbc3cd5bda89d222ba35d255f9614f9a55c
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-23 23:50:10 +0200

    imported icon module

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatIcon } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
@@ -10,7 +10,13 @@ import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIcon, MatButtonModule, MatTooltipModule, MatCardModule],
+  imports: [
+    MatIcon,
+    MatButtonModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatIconModule,
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
