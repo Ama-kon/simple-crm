3411634d08f99e1b4ad0acea78e2d35e3304b0c0
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-24 02:20:48 +0200

    usage of dummy data from database

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -6,7 +6,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 import { MatCardModule } from '@angular/material/card';
-
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -16,13 +17,24 @@ import { MatCardModule } from '@angular/material/card';
     MatTooltipModule,
     MatCardModule,
     MatIconModule,
+    CommonModule,
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
   user = new User();
+  allUsers: User[] = [];
+  private firestore: Firestore = inject(Firestore);
+
   constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    const userCollection = collection(this.firestore, 'users');
+    collectionData(userCollection).subscribe((changes: any) => {
+      this.allUsers = changes;
+    });
+  }
 
   openDialog(): void {
     this.dialog.open(DialogAddUserComponent, {
