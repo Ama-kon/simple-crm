964d73074eba4021ba8bece7f2563ce4d4c2bc3b
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-25 00:25:45 +0200

    added mat-menus for editing / imported needed modules

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -5,11 +5,20 @@ import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../../models/user.class';
 import { from } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [MatCardModule, CommonModule],
+  imports: [
+    MatCardModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+  ],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss',
 })
@@ -41,5 +50,13 @@ export class UserDetailsComponent {
         console.log('No such document!');
       }
     });
+  }
+
+  editAddressCard() {
+    console.log('openAdressDialog');
+  }
+
+  editNameCard() {
+    console.log('openNameDialog');
   }
 }
