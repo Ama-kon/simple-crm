7577953fe0739b182b5a97f67bad1163d7e98ada
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-24 23:29:48 +0200

    new route for user-details-component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+  },
+  {
+    path: 'user/:id',
+    component: UserDetailsComponent,
   },
 ];
