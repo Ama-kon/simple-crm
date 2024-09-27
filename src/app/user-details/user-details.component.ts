e5c2c03bab63299da2119cd0b9eabae993fd37ad
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-27 12:46:04 +0200

    format birth date from timestamp to DD.MM.YYYY

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -74,6 +74,14 @@ export class UserDetailsComponent implements OnInit, OnDestroy {
     });
   }
 
+  formatDate(birthDate: any): string {
+    const date = new Date(birthDate);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}`;
+  }
+
   editNameCard() {
     const userCopy = { ...this.currentUser[0] };
     const dialogRef = this.dialog.open(DialogEditNameComponent, {
