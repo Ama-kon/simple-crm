9e623795998beea816e9882cd83f88b9594d97a0
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-30 20:12:06 +0200

    new service to format dates to DD.MM.YYYY

---
+++ src/app/services/formatDate.service.ts
@@ -0,0 +1,9 @@
+export class FormatDateService {
+  formatDate(birthDate: any): string {
+    const date = new Date(birthDate);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}`;
+  }
+}
