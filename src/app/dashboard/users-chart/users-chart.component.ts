6dfed2e2c6d34bc8a600b7461863f37f07858594
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-30 22:13:11 +0200

    [feat] Add new component for chart - new monthly users

--- src/app/dashboard/users-chart/users-chart.component.ts
+++ src/app/dashboard/users-chart/users-chart.component.ts
@@ -1,13 +1,93 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-users-chart',
   standalone: true,
-  imports: [],
-  providers: [],
+  imports: [CommonModule, ChartModule],
   templateUrl: './users-chart.component.html',
-  styleUrl: './users-chart.component.scss',
+  styleUrls: ['./users-chart.component.scss'],
+  providers: [],
 })
-export class UsersChartComponent {
+export class UsersChartComponent implements OnInit {
+  data: any;
+
+  options: any;
   constructor() {}
+
+  ngOnInit() {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue(
+      '--text-color-secondary'
+    );
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.data = {
+      labels: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ],
+      datasets: [
+        {
+          label: '2023',
+          backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+          borderColor: documentStyle.getPropertyValue('--blue-500'),
+          data: [28, 42, 8, 39, 48, 46, 36, 39, 12, 18, 3, 24],
+        },
+        {
+          label: '2024',
+          backgroundColor: documentStyle.getPropertyValue('--pink-500'),
+          borderColor: documentStyle.getPropertyValue('--pink-500'),
+          data: [28, 48, 40, 45, 55, 27, 32, 65, 32],
+        },
+      ],
+    };
+
+    this.options = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.8,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor,
+          },
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+            font: {
+              weight: 500,
+            },
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+      },
+    };
+  }
 }
