38396646a709b878bb797d9d8be4ba951dd64670
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-30 20:16:19 +0200

    functions to sort from A-Z / Z-A

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,6 +10,7 @@ import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { FormatDateService } from '../services/formatDate.service';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -22,16 +23,21 @@ import { Subscription } from 'rxjs';
     CommonModule,
     RouterLink,
   ],
+  providers: [FormatDateService],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
 export class UserComponent implements OnInit, OnDestroy {
   user = new User();
   allUsers$: User[] = [];
+  aToZ: boolean = true;
   private userSubscription: Subscription;
   private firestore: Firestore = inject(Firestore);
 
-  constructor(public dialog: MatDialog) {}
+  constructor(
+    public dialog: MatDialog,
+    private formatDateService: FormatDateService
+  ) {}
 
   ngOnInit(): void {
     const userCollection = collection(this.firestore, 'users');
@@ -45,6 +51,62 @@ export class UserComponent implements OnInit, OnDestroy {
   openDialog(): void {
     this.dialog.open(DialogAddUserComponent, {
       data: {},
+    });
+  }
+
+  formatDate(birthDate: any): string {
+    return this.formatDateService.formatDate(birthDate);
+  }
+
+  sortUsers(criteria: string) {
+    if (this.aToZ) {
+      this.sortFromAToZ(criteria);
+      this.aToZ = false;
+    } else {
+      this.sortFromZToA(criteria);
+      this.aToZ = true;
+    }
+  }
+
+  sortFromAToZ(criteria: string) {
+    this.allUsers$.sort((a, b) => {
+      switch (criteria) {
+        case 'firstName':
+          return a.firstName.localeCompare(b.firstName);
+        case 'lastName':
+          return a.lastName.localeCompare(b.lastName);
+        case 'birthDate':
+          return (
+            new Date(a.birthDate).getTime() - new Date(b.birthDate).getTime()
+          );
+        case 'city':
+          return a.city.localeCompare(b.city);
+        case 'email':
+          return a.email.localeCompare(b.email);
+        default:
+          return 0;
+      }
+    });
+  }
+
+  sortFromZToA(criteria: string) {
+    this.allUsers$.sort((a, b) => {
+      switch (criteria) {
+        case 'firstName':
+          return b.firstName.localeCompare(a.firstName);
+        case 'lastName':
+          return b.lastName.localeCompare(a.lastName);
+        case 'birthDate':
+          return (
+            new Date(b.birthDate).getTime() - new Date(a.birthDate).getTime()
+          );
+        case 'city':
+          return b.city.localeCompare(a.city);
+        case 'email':
+          return b.email.localeCompare(a.email);
+        default:
+          return 0;
+      }
     });
   }
 
