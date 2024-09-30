0bbd60677d8b059e28a02bf8215ce31c55a8ff2f
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-30 20:13:08 +0200

    template for chart on dashboard component

---
+++ src/app/dashboard/users-chart/users-chart.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-chart',
+  standalone: true,
+  imports: [],
+  providers: [],
+  templateUrl: './users-chart.component.html',
+  styleUrl: './users-chart.component.scss',
+})
+export class UsersChartComponent {
+  constructor() {}
+}
