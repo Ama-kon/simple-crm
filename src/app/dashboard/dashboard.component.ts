c5f4a673c5b1e56a6c1063b10b3ac5eacca79c2a
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-09-30 22:14:17 +0200

    [feat] Add users chart, follow-ups chart, and customers by age chart to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { UsersChartComponent } from './users-chart/users-chart.component';
+import { FollowUpsComponent } from './follow-ups-chart/follow-ups.component';
+import { CustomersByAgeChartComponent } from './customers-by-age-chart/customers-by-age-chart.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    UsersChartComponent,
+    FollowUpsComponent,
+    CustomersByAgeChartComponent,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
