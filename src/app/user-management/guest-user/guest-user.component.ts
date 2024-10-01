f5f99964589ee2e4e72569de5db2754826630c2f
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-10-01 23:45:37 +0200

    [feat] add new component for guest registration, related to #1

---
+++ src/app/user-management/guest-user/guest-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './guest-user.component.html',
+  styleUrl: './guest-user.component.scss'
+})
+export class GuestUserComponent {
+
+}
