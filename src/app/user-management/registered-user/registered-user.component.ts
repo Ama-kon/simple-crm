03cddbf5f673b90409adec19b6a7fcfed0d397a1
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-10-01 23:44:59 +0200

    [feat] add new component for user registration, relatet to #1

---
+++ src/app/user-management/registered-user/registered-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registered-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './registered-user.component.html',
+  styleUrl: './registered-user.component.scss'
+})
+export class RegisteredUserComponent {
+
+}
