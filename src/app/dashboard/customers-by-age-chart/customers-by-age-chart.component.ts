50310b70f8ebfb813f8afdf64aea7c85c018b4f9
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-10-23 23:05:41 +0200

    [feat] use data from firebase to get clients ages and filter them into arrays, use as chart data

--- src/app/dashboard/customers-by-age-chart/customers-by-age-chart.component.ts
+++ src/app/dashboard/customers-by-age-chart/customers-by-age-chart.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { collection, Firestore, getDocs } from '@angular/fire/firestore';
 import { ChartModule } from 'primeng/chart';
 
 @Component({
@@ -10,11 +11,55 @@ import { ChartModule } from 'primeng/chart';
   styleUrl: './customers-by-age-chart.component.scss',
 })
 export class CustomersByAgeChartComponent implements OnInit {
-  data: any;
+  private firestore: Firestore = inject(Firestore);
 
+  data: any;
   options: any;
 
-  ngOnInit() {
+  users18To34: number = 0;
+  users35To49: number = 0;
+  users50To69: number = 0;
+  users70Plus: number = 0;
+
+  async ngOnInit() {
+    await this.checkAges();
+    this.showChart();
+  }
+
+  async checkAges() {
+    const userCollection = collection(this.firestore, 'standardData');
+    const querySnapshot = await getDocs(userCollection);
+    querySnapshot.forEach((doc) => {
+      const userData = doc.data();
+      const birthDate = userData['birthDate'];
+      const age = this.calculateAge(birthDate);
+      this.categorizeAge(age);
+    });
+  }
+
+  calculateAge(birthDate: any): number {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDiff = today.getMonth() - birthDateObj.getMonth();
+
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDateObj.getDate())
+    ) {
+      age--;
+    }
+    return age;
+  }
+
+  categorizeAge(age: number) {
+    if (age >= 18 && age <= 34) this.users18To34++;
+    else if (age >= 35 && age <= 49) this.users35To49++;
+    else if (age >= 50 && age <= 69) this.users50To69++;
+    else if (age >= 70) this.users70Plus++;
+  }
+
+  showChart() {
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--text-color');
 
@@ -22,17 +67,22 @@ export class CustomersByAgeChartComponent implements OnInit {
       labels: ['18-34', '35-49', '50-69', '70+'],
       datasets: [
         {
-          data: [42, 68, 18, 0],
+          data: [
+            this.users18To34,
+            this.users35To49,
+            this.users50To69,
+            this.users70Plus,
+          ],
           backgroundColor: [
-            documentStyle.getPropertyValue('--pink-500'),
-            documentStyle.getPropertyValue('--yellow-500'),
             documentStyle.getPropertyValue('--blue-500'),
+            documentStyle.getPropertyValue('--yellow-500'),
+            documentStyle.getPropertyValue('--pink-500'),
             documentStyle.getPropertyValue('--grey-500'),
           ],
           hoverBackgroundColor: [
-            documentStyle.getPropertyValue('--pink-400'),
-            documentStyle.getPropertyValue('--yellow-400'),
             documentStyle.getPropertyValue('--blue-400'),
+            documentStyle.getPropertyValue('--yellow-400'),
+            documentStyle.getPropertyValue('--pink-400'),
             documentStyle.getPropertyValue('--grey-400'),
           ],
         },
