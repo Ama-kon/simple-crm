31f46f095682f45719bbae7489b68bcc11710a8f
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-12-03 21:43:40 +0100

    [feat] add Property interface with relevant fields

---
+++ src/interfaces/property.interface.ts
@@ -0,0 +1,18 @@
+export interface Property {
+  id: string;
+  type: string;
+  streetNumber: string;
+  zipCity: string;
+  price: number;
+  sqmPrice: number;
+  yearbuilt: number;
+  livingSpace: number;
+  landArea?: number;
+  parking: string;
+  available: number; //timestamp
+  owner: string;
+  ownerPhone: number;
+  ownerMail: string;
+  ownerAdress: string;
+  imageUrls: string[];
+}
