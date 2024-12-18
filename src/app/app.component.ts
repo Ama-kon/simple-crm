f116a868142432d3f804919e6b9ed66ee8f50d2f
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-12-18 13:20:34 +0100

    [feat] include guest log in and log out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +6,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { StandardDataComponent } from './user-management/standard-data/standard-data.component';
 import { AuthPageComponent } from './auth-page/auth-page/auth-page.component';
+import { NgClass } from '@angular/common';
+import { AuthenticationService } from './services/authentication.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -18,11 +20,21 @@ import { AuthPageComponent } from './auth-page/auth-page/auth-page.component';
     MatIconModule,
     StandardDataComponent,
     AuthPageComponent,
+    NgClass,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'simple-crm';
-  constructor() {}
+  isGuest: boolean = false;
+  constructor(private authenticationService: AuthenticationService) {
+    this.authenticationService.isGuest$.subscribe((isGuest) => {
+      this.isGuest = isGuest;
+    });
+  }
+
+  logOut() {
+    this.authenticationService.logOut();
+  }
 }
