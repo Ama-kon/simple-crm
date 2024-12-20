ff36f21c255d9f6e11e442c370d4e5a7de2b7343
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-12-20 16:07:59 +0100

    [feat] -rename data service -delete guest data after logout

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { DataCopyService } from './data-copy.service';
+import { DataHandlingService } from './data-handling.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,17 +9,18 @@ export class AuthenticationService {
   private isGuestSubject = new BehaviorSubject<boolean>(false);
   isGuest$ = this.isGuestSubject.asObservable();
 
-  constructor(private dataCopyService: DataCopyService) {}
+  constructor(private dataHandlingService: DataHandlingService) {}
 
   setGuestMode(isGuest: boolean) {
     if (isGuest) {
-      this.dataCopyService.createGuestUserData();
+      this.dataHandlingService.createGuestUserData();
     }
     this.isGuestSubject.next(isGuest);
   }
 
   logOut() {
     this.setGuestMode(false);
+    this.dataHandlingService.deleteGuestData();
     console.log('abgemeldet');
   }
 }
