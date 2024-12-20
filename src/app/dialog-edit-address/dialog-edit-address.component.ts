c70fa5f9bf197211b184e8437b30f3b7cac701f4
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-12-20 16:13:51 +0100

    [feat] save edit of address (clients) with dynamic path handling, related to #1

--- src/app/dialog-edit-address/dialog-edit-address.component.ts
+++ src/app/dialog-edit-address/dialog-edit-address.component.ts
@@ -1,4 +1,12 @@
-import { Component, Inject, inject, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Inject,
+  inject,
+  Output,
+  EventEmitter,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
 import {
   MatDialogRef,
   MatDialogModule,
@@ -14,6 +22,8 @@ import { FormsModule } from '@angular/forms';
 import { Firestore, doc, updateDoc } from '@angular/fire/firestore';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CommonModule } from '@angular/common';
+import { AuthenticationService } from '../services/authentication.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-dialog-edit-address',
   standalone: true,
@@ -31,30 +41,45 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dialog-edit-address.component.html',
   styleUrl: './dialog-edit-address.component.scss',
 })
-export class DialogEditAddressComponent {
+export class DialogEditAddressComponent implements OnDestroy {
   user: User;
   loading = false;
   private firestore: Firestore = inject(Firestore);
+  private isGuest: boolean = false;
+  private subscription: Subscription;
   @Output() userUpdated = new EventEmitter<void>();
+
   constructor(
     public dialog: MatDialogRef<DialogEditAddressComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { user: User; userId: string }
+    @Inject(MAT_DIALOG_DATA) public data: { user: User; userId: string },
+    private authService: AuthenticationService
   ) {
     this.user = this.data.user;
+    this.subscription = this.authService.isGuest$.subscribe(
+      (isGuest) => (this.isGuest = isGuest)
+    );
   }
 
   saveEdit() {
     this.loading = true;
-    const userDocRef = doc(this.firestore, 'users', this.data.userId);
+    const collectionPath = this.isGuest
+      ? `guest/guestDoc/standardData`
+      : 'standardData';
+
+    const userDocRef = doc(this.firestore, collectionPath, this.data.userId);
+
     updateDoc(userDocRef, { ...this.user }).then(() => {
       this.loading = false;
       this.userUpdated.emit();
       this.closeDialog();
-      this.loading = false;
     });
   }
 
   closeDialog() {
     this.dialog.close();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
