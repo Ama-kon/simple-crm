c482b354ec47f945d3e1fd8094e06c6e58cc5062
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2025-01-02 17:44:31 +0100

    [feat] dynamic path handling based on auth state

--- src/app/services/file-upload.service.ts
+++ src/app/services/file-upload.service.ts
@@ -5,11 +5,21 @@ import {
   uploadBytes,
   getDownloadURL,
 } from '@angular/fire/storage';
+import { firstValueFrom } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileUploadService {
+  private isGuest: boolean;
+
+  constructor(private authService: AuthenticationService) {
+    this.authService.isGuest$.subscribe((value) => {
+      this.isGuest = value;
+      console.log('isGuest:', this.isGuest);
+    });
+  }
   private storage: Storage = inject(Storage);
 
   async uploadImage(file: File, path: string): Promise<string> {
@@ -22,8 +32,11 @@ export class FileUploadService {
     propertyId: string,
     files: File[]
   ): Promise<string[]> {
+    const isGuest = await firstValueFrom(this.authService.isGuest$);
+    const basePath = isGuest ? 'guest/properties' : 'properties';
+
     const uploadPromises = files.map((file, index) => {
-      const path = `properties/${propertyId}/image_${index}_${file.name}`;
+      const path = `${basePath}/${propertyId}/image_${index}_${file.name}`;
       return this.uploadImage(file, path);
     });
     return Promise.all(uploadPromises);
